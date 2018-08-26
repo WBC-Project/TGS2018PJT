632e234086533271f0149085ab37d4ba62e2bde1
小林怜登 <reito86chikuwa@yahoo.co.jp>
2018-08-27 08:31:42 +0900

    add BattleDamage.cs & hitpoint deathFlag in BattleActor.cs

---
+++ Assets/TGS/Scripts/Domain/Battle/Actor/BattleDamage.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace TGS.Domain.Battle.Actor
+{
+
+    /// <summary>
+    /// ダメージ処理のインターフェース
+    /// </summary>
+    public interface IBattleDamage
+    {
+        /// <summary>
+        /// ユニークID
+        /// </summary>
+        /// <value></value>
+        string Guid { get; set; }
+
+        /// <summary>
+        /// ゲームオブジェクト
+        /// </summary>
+        /// <value></value>
+        GameObject gameObject { get; }
+        
+        /// <summary>
+        /// 初期化
+        /// </summary>
+        void Initialize();
+    }
+
+
+
+    
+    /// <summary>
+    /// ダメージ処理
+    /// </summary>
+}
