e7ef538371dedf9fad35595cd98f3072e2a52b07
小林怜登 <reito86chikuwa@yahoo.co.jp>
2018-09-12 03:52:29 +0900

    add DebugMenu

---
+++ Assets/TGS/Scripts/Domain/Menu/DebugMenu/DebugMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+/// <summary>
+/// ゲーム開始時等、デバッグメニューを出したい任意のタイミングで、このスクリプトを呼んでください。
+/// </summary>
+
+public class DebugMenu : MonoBehaviour
+{
+	private void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+	}
+}
